7b3710e0cb9cf013f556c5aca95c7354977bc4fd
SimulacraAtTanagra <shaneayers1@gmail.com>
2021-04-07 18:17:49 -0400

    updated AccessibilityChallenge.py

--- AccessibilityChallenge.py
+++ AccessibilityChallenge.py
@@ -4,13 +4,15 @@ Created on Wed Apr  7 16:31:23 2021
 
 @author: shane
 """
-from admin import write_json,read_json,retrieve_json, encrypt, decrypt
+from admin import read_json, decrypt,install
 import ast
 import os
 from time import sleep
 
+
+
 def main():
-    passcode=input("Please enter your last name, as it appears on linkedin, to begin.")
+    passcode=input("Please enter your last name, as it appears on linkedin, to begin.\n")
     print(decrypt(ast.literal_eval(read_json(os.path.join(os.getcwd(),'begin.json')),passcode.upper()*3,16)))
     while True:
         if gameloop():
@@ -21,7 +23,7 @@ def gameloop():
     print(decrypt(ast.literal_eval(read_json(os.path.join(os.getcwd(),'program.json')),"What I thought I'd do was",16)))
     print('\n\n\n\n')
     sleep(5)
-    solution=input("What line is missing from this program to make it work?")
+    solution=input("Type the line that is missing from this program to make it work.\n")
     if "i quit" in solution.strip().lower():
         print(decrypt(ast.literal_eval(read_json(os.path.join(os.getcwd(),'defeat.json')),solution.strip().lower()*3,16)))
         return(True)
@@ -36,6 +38,5 @@ def gameloop():
         return(False)
         
 if __name__=="__main__":
+    install('pycryptodome')
     main()
-    
-
