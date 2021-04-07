022ff747edd75a13a515494b5a16e010941922e2
SimulacraAtTanagra <shaneayers1@gmail.com>
2021-04-07 18:29:28 -0400

    Daily automated update

--- AccessibilityChallenge.py
+++ AccessibilityChallenge.py
@@ -4,7 +4,7 @@ Created on Wed Apr  7 16:31:23 2021
 
 @author: shane
 """
-from admin import read_json, decrypt,install
+from src.admin import read_json, decrypt,install
 import ast
 import os
 from time import sleep
@@ -13,22 +13,22 @@ from time import sleep
 
 def main():
     passcode=input("Please enter your last name, as it appears on linkedin, to begin.\n")
-    print(decrypt(ast.literal_eval(read_json(os.path.join(os.getcwd(),'begin.json')),passcode.upper()*3,16)))
+    print(decrypt(ast.literal_eval(read_json(os.path.join(os.getcwd(),'begin.json'))),passcode.upper()*3,16))
     while True:
         if gameloop():
             break
 
 def gameloop():
     print("Here find the incomplete progrm:")
-    print(decrypt(ast.literal_eval(read_json(os.path.join(os.getcwd(),'program.json')),"What I thought I'd do was",16)))
+    print(decrypt(ast.literal_eval(read_json(os.path.join(os.getcwd(),'program.json'))),"What I thought I'd do was",16))
     print('\n\n\n\n')
     sleep(5)
     solution=input("Type the line that is missing from this program to make it work.\n")
     if "i quit" in solution.strip().lower():
-        print(decrypt(ast.literal_eval(read_json(os.path.join(os.getcwd(),'defeat.json')),solution.strip().lower()*3,16)))
+        print(decrypt(ast.literal_eval(read_json(os.path.join(os.getcwd(),'defeat.json'))),solution.strip().lower()*3,16))
         return(True)
     try:
-        xyz=decrypt(ast.literal_eval(read_json(os.path.join(os.getcwd(),'message.json')),solution,16))
+        xyz=decrypt(ast.literal_eval(read_json(os.path.join(os.getcwd(),'message.json'))),solution,16)
         if "Rubik" in xyz:
             print('Congratulations!')
             print(xyz)
